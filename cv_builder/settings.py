847844ab065ce822cd45c60ebe39c8c71a153bac
José Alfredo Arroyo Gómez <92402209+AlfredoAG025@users.noreply.github.com>
2025-12-29 16:52:13 -0600

    Set up django-htmx

--- cv_builder/settings.py
+++ cv_builder/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_htmx',
     'apps.resumes',
 ]
 
@@ -56,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_htmx.middleware.HtmxMiddleware',
 ]
 
 ROOT_URLCONF = 'cv_builder.urls'
