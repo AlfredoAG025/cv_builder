b3bb2b3e11c4c62a7fb798919139ba5a4e9b24a1
José Alfredo Arroyo Gómez <92402209+AlfredoAG025@users.noreply.github.com>
2025-12-29 15:59:03 -0600

    Preview resume with htmx

--- apps/resumes/urls.py
+++ apps/resumes/urls.py
@@ -5,5 +5,6 @@ from apps.resumes.views import create_cv
 app_name = 'resumes'
 
 urlpatterns = [
-    path('editor/', create_cv, name='create')
+    path('editor/', create_cv, name='create'),
+    path('preview/', create_cv, name='resume_preview'),
 ]
--- apps/resumes/views.py
+++ apps/resumes/views.py
@@ -2,9 +2,25 @@ from django.shortcuts import render
 
 from apps.resumes.forms import ResumeForm
 
-def create_cv(request):
-    form = ResumeForm()
-    context = {
-        'form': form,
-    }
-    return render(request, 'apps/resumes/create.html', context)
+def resume_create(request):
+    form = ResumeForm(request.POST)
+
+    if form.is_valid():
+        form.save()
+        return redirect("resume-list")
+
+    return render(request, "apps/resumes/create.html", {"form": form})
+
+def resume_preview(request):
+    form = ResumeForm(request.POST)
+
+    if form.is_valid():
+        resume = form.cleaned_data
+    else:
+        resume = {}
+
+    return render(
+        request,
+        "apps/resumes/preview.html",
+        {"resume": resume}
+    )
